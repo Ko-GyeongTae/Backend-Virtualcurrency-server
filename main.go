c1ea74ef7f5c5a7b1bc8b3d94a6b2f9e852dd69d
Ko-GyeongTae <kokt0203@naver.com>
2021-07-13 13:39:19 +0900

    <EDIT> set

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "github.com/Ko-GyeongTae/Backend-Nomadcoin-server/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Ko-GyeongTae/Backend-Nomadcoin-server/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s", b)
+}
 
 func main() {
-	explorer.Start()
+	fmt.Printf("Listening on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
