14f1234422699f7ca766463653c26b3b704e358e
Ko-GyeongTae <kokt0203@naver.com>
2021-07-20 22:30:11 +0900

    <EDIT> variable port

--- explorer/explorer.go
+++ explorer/explorer.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/Ko-GyeongTae/Backend-Nomadcoin-server/blockchain"
+	"github.com/Ko-GyeongTae/Backend-Virtualcurrency-server/blockchain"
 )
 
 const (
@@ -38,11 +38,11 @@ func add(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func Start() {
+func Start(port int) {
 	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
 	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
 	http.HandleFunc("/", home)
 	http.HandleFunc("/add", add)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	fmt.Printf("Listening on http://localhost:%d\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
