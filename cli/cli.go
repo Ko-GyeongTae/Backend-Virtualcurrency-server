07b1ed4388cf0f8879deef829b5d8244d77e6394
KoGyeongtae <kokt0203@naver.com>
2021-07-22 19:36:50 +0900

    <EDIT> cli

--- cli/cli.go
+++ cli/cli.go
@@ -1,3 +1,29 @@
 package cli
 
-func Start()
+import (
+	"fmt"
+	"os"
+)
+
+func usage() {
+	fmt.Printf("Welcome to Neon Coid\n\n")
+	fmt.Printf("Please se the following Commands:\n\n")
+	fmt.Printf("explorer:	Start the HTML Explorer\n")
+	fmt.Printf("rest:		Start the REST API (recommended)\n\n")
+	os.Exit(0)
+}
+
+func Start() {
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
+}
