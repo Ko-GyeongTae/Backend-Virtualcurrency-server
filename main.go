37c7f8593d6fdcb301e81b6df987cac83bab8a10
KoGyeongtae <kokt0203@naver.com>
2021-07-27 08:20:46 +0900

    <ADD> defer func to save & close DB

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/Ko-GyeongTae/Backend-Virtualcurrency-server/cli"
+	"github.com/Ko-GyeongTae/Backend-Virtualcurrency-server/db"
 )
 
 func main() {
+	defer db.Close()
 	cli.Start()
 }
