8f2412582ff6d5dd6e7fdcb3a182f2e3303fe06a
KoGyeongtae <kokt0203@naver.com>
2021-07-27 20:21:25 +0900

    <EDIT> TxIn field & <ADD> UTx type

--- blockchain/block.go
+++ blockchain/block.go
@@ -45,7 +45,7 @@ func (b *Block) mine() {
 	for {
 		b.Timestamp = int(time.Now().Unix())
 		hash := utils.Hash(b)
-		fmt.Printf("\n\n: %s\nHash: %s\nNonce: %d\n\n", target, hash, b.Nonce)
+		fmt.Printf("Target: %s\nHash: %s\nNonce: %d\n\n", target, hash, b.Nonce)
 		if strings.HasPrefix(hash, target) {
 			b.Hash = hash
 			break
